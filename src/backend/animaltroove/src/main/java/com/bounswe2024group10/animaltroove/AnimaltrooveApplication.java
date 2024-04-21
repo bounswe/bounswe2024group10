5dd92a0e678fd175b566ee391fe8adbb104c56da
melihakpinar <mesutmelihakpinar@gmail.com>
2024-04-21 14:10:41 +0300

    Initialize Animal Troove backend service application files.

---
+++ src/backend/animaltroove/src/main/java/com/bounswe2024group10/animaltroove/AnimaltrooveApplication.java
@@ -0,0 +1,13 @@
+package com.bounswe2024group10.animaltroove;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimaltrooveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimaltrooveApplication.class, args);
+	}
+
+}
