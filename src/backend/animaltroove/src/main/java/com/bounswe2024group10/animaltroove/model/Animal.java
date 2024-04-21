818a19df196bd15a36321244b84aed0cadf85d97
melihakpinar <mesutmelihakpinar@gmail.com>
2024-04-21 15:54:10 +0300

    Add database credentials format to application properties

---
+++ src/backend/animaltroove/src/main/java/com/bounswe2024group10/animaltroove/model/Animal.java
@@ -0,0 +1,50 @@
+package com.bounswe2024group10.animaltroove.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.sql.Blob;
+
+@Entity
+@Table(name = "animal")
+public class Animal {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "region", nullable = false)
+    private String region;
+
+    @Column(name = "eating_type")
+    private String eatingType;
+
+    @Column(name = "latin_name")
+    private String latinName;
+
+    @Column(name = "common_name", nullable = false)
+    private String commonName;
+
+    @Column(name = "average_life")
+    private Integer averageLife;
+
+    @Column(name = "picture")
+    private Blob picture;
+
+    @Column(name = "description", length = 100)
+    private String description;
+
+    public Animal() {
+        // Default constructor
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
