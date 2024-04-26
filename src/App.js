d5152c8eed44a8fac2bac36a02f2752aa68733f1
Yusuf Kağan Çiçekdağ <128151393+Pqrq@users.noreply.github.com>
2024-04-27 01:01:08 +0300

    Add files via upload
    Main contents are committed.
    
    Remaining directories inside this src are:
    .expo
    .idea
    node_modules
    
    which I am not sure whether they should be committed or not 
    (also they are too big to commit)

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './pages/HomeScreen';
+import DetailsScreen from './pages/DetailsScreen';
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
