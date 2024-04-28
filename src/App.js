c724a337b77bd7acb0c9a81ee228a9a7d08f04c8
TheRealLowyer <atli.yasin00@gmail.com>
2024-04-28 15:03:46 +0300

    Main-Login-Signup pages added

--- src/App.js
+++ src/App.js
@@ -3,15 +3,25 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './pages/HomeScreen';
 import DetailsScreen from './pages/DetailsScreen';
+import SettingsScreen from './pages/SettingsScreen';
+import SearchB from './pages/SearchB' ; 
+import MainScreen from './pages/MainScreen';
+import LoginScreen from './pages/LoginScreen';
+import SignupScreen from './pages/SignUpScreen';
 
 const Stack = createStackNavigator();
 
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Navigator initialRouteName="Main">
+        <Stack.Screen name = "Main" component={MainScreen} />
+        <Stack.Screen name = "Login" component={LoginScreen} />
+        <Stack.Screen name = "Sign-Up" component={SignupScreen}  />
+        <Stack.Screen name="Home" component={HomeScreen} options={{ headerLeft: null }} />
         <Stack.Screen name="Details" component={DetailsScreen} />
+        <Stack.Screen name = "Settings" component={SettingsScreen} options={{ headerLeft: null }} />
+        <Stack.Screen name = "Search" component={SearchB}  options={{ headerLeft: null }} />   
       </Stack.Navigator>
     </NavigationContainer>
   );
