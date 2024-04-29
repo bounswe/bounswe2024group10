f3533153f1f98a3196e024bca7e8fb9569460431
Nazlıcan Aka <nazlicanaka421@gmail.com>
2024-04-29 11:14:57 +0300

    create login and sign up pages

---
+++ src/web/animal-trove/src/pages/LoginSignUp.js
@@ -0,0 +1,41 @@
+// Login.js
+import React , { useState }from "react";
+import "./LoginSignUp.css";
+
+import user_icon from "../Assets/person.png"
+import email_icon from "../Assets/email.png"
+import password_icon from "../Assets/password.png"
+
+export default function Login() {
+  const[action, setAction] = useState("Sign Up");
+  return (
+
+    <div className='container'>
+      <div className="header">
+        <div className="text">{action}</div>
+        <div className="underline"></div>
+      </div>
+      <div className="inputs">
+        {action==="Login"?<div></div>:<div className="input">
+          <img src={user_icon} alt="" />
+          <input type="text" placeholder="Name"/>
+        </div>}
+        
+        <div className="input">
+          <img src={email_icon} alt="" />
+          <input type="email" placeholder="Email"/>
+        </div>
+        <div className="input">
+          <img src={password_icon} alt="" />
+          <input type="password" placeholder="Password"/>
+        </div>
+      </div>
+      {action==="Sign Up"?<div></div>:<div className="forgot-password">Lost Password <span>Click Here!</span></div>}
+      <div className="submit-container">
+        <div className={action==="Login"?"submit gray":"submit"} onClick={()=>{setAction("Sign Up")}}>Sign Up</div>
+        <div className={action==="Sign Up"?"submit gray":"submit"}onClick={()=>{setAction("Login")}}>Login</div>
+      </div>
+    </div>
+
+  );
+}
