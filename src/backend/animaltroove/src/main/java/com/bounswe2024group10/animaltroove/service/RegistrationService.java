4053389c07cb983968602ff2fa2a707069862117
melihakpinar <mesutmelihakpinar@gmail.com>
2024-05-03 21:11:17 +0300

    Fix error handling for IllegalArgumentException

--- src/backend/animaltroove/src/main/java/com/bounswe2024group10/animaltroove/service/RegistrationService.java
+++ src/backend/animaltroove/src/main/java/com/bounswe2024group10/animaltroove/service/RegistrationService.java
@@ -26,11 +26,12 @@ public class RegistrationService {
             return new RegisterResponse(false, "Invalid email address.");
         }
         RegisteredUser newUser = new RegisteredUser(request.getUserName(), request.getEmail(), request.getPassword(), request.getName(), request.getBirthday(), request.getBio(), request.getProfilePicture());
-        if (userRepository.save(newUser) == null) {
-            return new RegisterResponse(false, "An error occurred while saving the user to the database.");
-        } else {
-            return new RegisterResponse(true, "User registered successfully.");
+        try {
+            userRepository.save(newUser);
+        } catch (IllegalArgumentException e) {
+            return new RegisterResponse(false, "Invalid user data.");
         }
+        return new RegisterResponse(true, "User registered successfully.");
     }
 
     private boolean isValidEmail(String email) {
