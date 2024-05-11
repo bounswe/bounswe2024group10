2156146bc7fb42a1b2a7f722a7742fcd1f9a8fe4
melihakpinar <mesutmelihakpinar@gmail.com>
2024-05-11 17:57:10 +0300

    Store passwords hashed

--- src/backend/animaltroove/src/main/java/com/bounswe2024group10/animaltroove/service/LoginService.java
+++ src/backend/animaltroove/src/main/java/com/bounswe2024group10/animaltroove/service/LoginService.java
@@ -5,8 +5,8 @@ import com.bounswe2024group10.animaltroove.dto.LoginResponse;
 import com.bounswe2024group10.animaltroove.model.RegisteredUser;
 import com.bounswe2024group10.animaltroove.repository.RegisteredUserRepository;
 
-import org.apache.commons.logging.Log;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -15,14 +15,16 @@ public class LoginService {
     @Autowired
     private RegisteredUserRepository userRepository;
 
+    private BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
     public LoginResponse loginUser(LoginRequest request) {
         if (request.getUserName() == null || request.getPassword() == null) {
             return new LoginResponse(false, "Username or password cannot be null", null, null);
         }
-        if (userRepository.findByUserNameAndPassword(request.getUserName(), request.getPassword()) == null) {
-            return new LoginResponse(false, "Username or password is incorrect", null, null);
+        RegisteredUser user = userRepository.findByUserName(request.getUserName());
+        if (user == null || !passwordEncoder.matches(request.getPassword(), user.getPassword())) {
+            return new LoginResponse(false, "Invalid username or password", null, null);
         }
-        RegisteredUser user = userRepository.findByUserNameAndPassword(request.getUserName(), request.getPassword());
         String token = user.getUserName() + user.getPassword();
         return new LoginResponse(true, "Login successful", token, user.getUserName());
     }
