a667a34b41b936d4c94a31b7a838e240e903f3ad
melihakpinar <mesutmelihakpinar@gmail.com>
2024-05-16 20:22:56 +0300

    Add @Service annotation to comment service.

--- src/backend/animaltroove/src/main/java/com/bounswe2024group10/animaltroove/service/CommentService.java
+++ src/backend/animaltroove/src/main/java/com/bounswe2024group10/animaltroove/service/CommentService.java
@@ -10,7 +10,9 @@ import com.bounswe2024group10.animaltroove.repository.PostRepository;
 import com.bounswe2024group10.animaltroove.repository.RegisteredUserRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class CommentService {
 
     @Autowired
