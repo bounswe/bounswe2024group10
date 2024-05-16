a227779d2e62281ef1b4555f58d7642a0c27ef03
melihakpinar <mesutmelihakpinar@gmail.com>
2024-05-16 16:22:54 +0300

    Implement findByUserNameContaining function

--- src/backend/animaltroove/src/main/java/com/bounswe2024group10/animaltroove/repository/RegisteredUserRepository.java
+++ src/backend/animaltroove/src/main/java/com/bounswe2024group10/animaltroove/repository/RegisteredUserRepository.java
@@ -1,5 +1,7 @@
 package com.bounswe2024group10.animaltroove.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.Query;
 import com.bounswe2024group10.animaltroove.model.RegisteredUser;
 import org.springframework.data.repository.CrudRepository;
@@ -9,5 +11,7 @@ import org.springframework.stereotype.Repository;
 public interface RegisteredUserRepository extends CrudRepository<RegisteredUser, Integer> {
     RegisteredUser findByUserNameAndPassword(String username, String password);
     RegisteredUser findByUserName(String username);
+    @Query("SELECT u FROM RegisteredUser u WHERE u.userName LIKE %?1%")
+    List<RegisteredUser> findByUserNameContaining(String keyword);
     RegisteredUser findByEmail(String email);
 }
