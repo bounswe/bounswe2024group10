eba90964efd406fde0f596da102c891ceb28b598
Onur Kafkas <onurkafkas2001@gmail.com>
2024-05-16 17:54:24 +0300

    Implemented dislike service/controller/dto/repository similar to like.

---
+++ src/backend/animaltroove/src/main/java/com/bounswe2024group10/animaltroove/service/DislikeService.java
@@ -0,0 +1,36 @@
+package com.bounswe2024group10.animaltroove.service;
+
+import com.bounswe2024group10.animaltroove.dto.DislikeRequest;
+import com.bounswe2024group10.animaltroove.dto.DislikeResponse;
+import com.bounswe2024group10.animaltroove.dto.LikeRequest;
+import com.bounswe2024group10.animaltroove.dto.LikeResponse;
+import com.bounswe2024group10.animaltroove.model.Disliked;
+import com.bounswe2024group10.animaltroove.model.Liked;
+import com.bounswe2024group10.animaltroove.repository.RegisteredUserRepository;
+import com.bounswe2024group10.animaltroove.repository.DislikedRepository;
+import com.bounswe2024group10.animaltroove.repository.LikedRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DislikeService {
+    
+    @Autowired
+    private DislikedRepository dislikedRepository;
+
+    @Autowired
+    private RegisteredUserRepository registeredUserRepository;
+
+    public DislikeResponse dislikePost(DislikeRequest request) {
+        if (registeredUserRepository.findByUserName(request.getUsername()) == null) {
+            return new DislikeResponse(false, "User not found");
+        }
+        if (dislikedRepository.existsByUsernameAndPostID(request.getUsername(), request.getPostID())) {
+            return new DislikeResponse(false, "Post already disliked");
+        }
+        dislikedRepository.save(new Disliked(request.getUsername(), request.getPostID()));
+        return new DislikeResponse(true, "Post disliked");
+    }
+
+}
