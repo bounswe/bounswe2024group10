035ad5c78f3441477e61d52cb99ecb0a10159598
Huseyin Karatas <karatashuseyin2016@gmail.com>
2024-05-17 21:29:51 +0300

    Api baseUrl Fix

--- src/web/animal-trove/src/services/api.js
+++ src/web/animal-trove/src/services/api.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  // baseURL: "http://165.22.95.135:8080/api",
-  baseURL: "http://localhost:8080/api",
+  baseURL: "http://165.22.95.135:8080/api",
 });
 
 export default api;
