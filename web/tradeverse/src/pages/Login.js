62b59335c9daa9516cc1930b05f5de2398539a13
Yasin Atlı <atli.yasin00@gmail.com>
2024-10-19 01:54:53 +0300

    Login page created

---
+++ web/tradeverse/src/pages/Login.js
@@ -0,0 +1,107 @@
+import { useReducer, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { AuthData } from "../auth/AuthWrapper";
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import './styles/Login.css';
+
+
+const Login = () => {
+    const navigate = useNavigate();
+    const { login } = AuthData();
+    const [formData, setFormData] = useReducer((formData, newItem) => {
+        return { ...formData, ...newItem };
+    }, { userName: "", password: "" });
+    const [errorMessage, setErrorMessage] = useState(null);
+
+    const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+
+    const doLogin = async () => {
+
+          if (!formData.userName) {
+               setErrorMessage('Username cannot be null'); // Warning message for blank username
+               return;
+          }
+     
+          if (!formData.password) {
+               setErrorMessage('Password cannot be null'); // Warning message for blank password
+               return;
+          }
+        setErrorMessage(null); // Clear previous error message
+        await delay(1000); // Wait for 1 second
+        try {
+            const response = await login(formData.userName, formData.password);
+            
+            // Check the response for success or error
+            if (response.success) {
+               toast("Login successful!");
+               
+                
+            }
+            navigate("/home"); // Redirect after success message
+        } catch (error) {
+          // Display the error message sent from the backend
+          setErrorMessage(error);
+     }
+    };
+
+    const handleKeyDown = (event) => {
+        if (event.key === "Enter") {
+            doLogin();
+        }
+    };
+
+    return (
+        <div className="page">
+            <div className="inputs">
+                <div>
+                <h2>We’ve missed you!</h2>
+                <span>More than 150 subforums are waiting for your wise suggestions!</span> 
+                <div className="input">
+                    
+                    <input
+                        value={formData.userName}
+                        onChange={(e) => setFormData({ userName: e.target.value })}
+                        onKeyDown={handleKeyDown}
+                        type="text"
+                        placeholder="Enter your username"
+                        style={{ marginTop: '10px', height: '40px' }}
+                    />
+                </div>
+
+                <div className="input">
+                    <input
+                        value={formData.password}
+                        onChange={(e) => setFormData({ password: e.target.value })}
+                        onKeyDown={handleKeyDown}
+                        type="password"
+                        placeholder="Enter your password"
+                        style={{ marginTop: '10px', height: '40px' }}
+                    />
+                </div>
+
+                <div >
+                    <button className="button"onClick={doLogin}>Log in</button>
+                </div>
+
+                {/* Display error message */}
+                {errorMessage && (
+                    <div style={{ color: 'red', marginTop: '10px' }}>
+                        {errorMessage}
+                    </div>
+                )}
+                </div>
+            </div>
+            <div className="image-container">
+                <div style={{objectFit:"cover",width:"100%"}}>
+                <img 
+                src="/background.jpg" 
+                />
+                </div>
+            </div>
+            <ToastContainer />
+        </div>
+    );
+};
+
+export default Login;
