35f887c256d605b5d005c13630e14217138d1d10
karatashsyn <81769362+karatashsyn@users.noreply.github.com>
2024-10-20 15:41:11 +0300

    feat: Add mock data for explore screen

--- mobile/tradeverse/screens/explore-root/index.jsx
+++ mobile/tradeverse/screens/explore-root/index.jsx
@@ -18,6 +18,7 @@ export default function ExploreRootScreen() {
         <SearchBar />
       </PaddedContainer>
       <Tabs selectedTab={selectedTab} setSelectedTab={setSelectedTab} />
+      {}
     </GlobalScreen>
   );
 }
