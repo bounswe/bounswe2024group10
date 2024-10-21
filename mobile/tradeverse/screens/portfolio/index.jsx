2c8d249dc826ddc1739a28fc184379723faaf3e8
Pqrq <128151393+Pqrq@users.noreply.github.com>
2024-10-21 04:15:20 +0300

    refactor: Refactor Styling

--- mobile/tradeverse/screens/portfolio/index.jsx
+++ mobile/tradeverse/screens/portfolio/index.jsx
@@ -1,56 +1,61 @@
-import React from 'react';
-import { View, Text, ScrollView, StyleSheet, TouchableOpacity } from 'react-native';
-import GlobalScreen from "../../components/ui/global-screen"
-import FullScrollView from "../../components/ui/full-scroll-view"
+import React from "react";
+import {
+  View,
+  Text,
+  ScrollView,
+  StyleSheet,
+  TouchableOpacity,
+} from "react-native";
+import GlobalScreen from "../../../components/ui/global-screen";
+import FullScrollView from "../../../components/ui/full-scroll-view";
+import { SIZE_CONSTANT } from "../../../constants/theme";
 // Mock Data
 const portfolioData = [
-  { name: 'BTC', value: '0.005' },
-  { name: 'USD', value: '$31.82' },
-  { name: 'EUR', value: '€52.96' },
-  { name: 'JPY', value: '¥4000.50' },
-  { name: 'BTC', value: '0.005' },
-  { name: 'USD', value: '$31.82' },
-  { name: 'EUR', value: '€52.96' },
-  { name: 'JPY', value: '¥4000.50' },
-  { name: 'BTC', value: '0.005' },
-  { name: 'USD', value: '$31.82' },
-  { name: 'EUR', value: '€52.96' },
-  { name: 'JPY', value: '¥4000.50' },
-  { name: 'BTC', value: '0.005' },
-  { name: 'USD', value: '$31.82' },
-  { name: 'EUR', value: '€52.96' },
-  { name: 'JPY', value: '¥4000.50' },
+  { name: "BTC", value: "0.005" },
+  { name: "USD", value: "$31.82" },
+  { name: "EUR", value: "€52.96" },
+  { name: "JPY", value: "¥4000.50" },
+  { name: "BTC", value: "0.005" },
+  { name: "USD", value: "$31.82" },
+  { name: "EUR", value: "€52.96" },
+  { name: "JPY", value: "¥4000.50" },
+  { name: "BTC", value: "0.005" },
+  { name: "USD", value: "$31.82" },
+  { name: "EUR", value: "€52.96" },
+  { name: "JPY", value: "¥4000.50" },
+  { name: "BTC", value: "0.005" },
+  { name: "USD", value: "$31.82" },
+  { name: "EUR", value: "€52.96" },
+  { name: "JPY", value: "¥4000.50" },
   // You can add more assets as needed
 ];
 
-
 const PortfolioScreen = () => {
   return (
-  <GlobalScreen>
-  <FullScrollView>
-    <View style={styles.container}>
-
-      <View style={styles.titleBlock}>
-        <Text style={styles.title}>My Portfolio</Text>
-        <TouchableOpacity style={styles.addButton}>
-          <Text style={styles.addButtonText}>+</Text>
-        </TouchableOpacity>
-      </View>
-
-      {/* Scrollable Portfolio Block */}
-      <ScrollView contentContainerStyle={styles.scrollViewContent}>
-        <View style={styles.portfolioBlock}>
-          {portfolioData.map((asset, index) => (
-            <TouchableOpacity key={index} style={styles.assetBlock}>
-              <Text style={styles.assetName}>{asset.name}</Text>
-              <Text style={styles.assetValue}>{asset.value}</Text>
+    <GlobalScreen>
+      <FullScrollView>
+        <View style={styles.container}>
+          <View style={styles.titleBlock}>
+            <Text style={styles.title}>My Portfolio</Text>
+            <TouchableOpacity style={styles.addButton}>
+              <Text style={styles.addButtonText}>+</Text>
             </TouchableOpacity>
-          ))}
+          </View>
+
+          {/* Scrollable Portfolio Block */}
+          <ScrollView contentContainerStyle={styles.scrollViewContent}>
+            <View style={styles.portfolioBlock}>
+              {portfolioData.map((asset, index) => (
+                <TouchableOpacity key={index} style={styles.assetBlock}>
+                  <Text style={styles.assetName}>{asset.name}</Text>
+                  <Text style={styles.assetValue}>{asset.value}</Text>
+                </TouchableOpacity>
+              ))}
+            </View>
+          </ScrollView>
         </View>
-      </ScrollView>
-    </View>
+      </FullScrollView>
     </GlobalScreen>
-    </FullScrollView>
   );
 };
 
@@ -58,55 +63,58 @@ const PortfolioScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#EDE7F6', // Light purple background
-    padding: 20,
   },
   titleBlock: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
     marginBottom: 20,
   },
   title: {
     fontSize: 24,
-    fontWeight: 'bold',
-    color: '#4A148C', // Dark purple
+    fontWeight: "bold",
+    color: "#4A148C", // Dark purple
   },
   addButton: {
-    backgroundColor: '#7B1FA2', // Medium purple
-    padding: 10,
+    backgroundColor: "#7B1FA2", // Medium purple
+    width: 48,
+    height: 48,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
     borderRadius: 25,
   },
+
   addButtonText: {
-    fontSize: 18,
-    color: '#FFFFFF', // White text
+    fontSize: SIZE_CONSTANT * 2,
+    color: "#FFFFFF", // White text
   },
   scrollViewContent: {
     paddingBottom: 30,
   },
   portfolioBlock: {
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    justifyContent: 'space-between',
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "space-between",
   },
   assetBlock: {
-    backgroundColor: '#D1C4E9', // Lighter purple
-    width: '48%',
+    backgroundColor: "#D1C4E9", // Lighter purple
+    width: "48%",
     height: 100,
     borderRadius: 10,
     padding: 10,
-    justifyContent: 'space-between',
+    justifyContent: "space-between",
     marginBottom: 20,
   },
   assetName: {
     fontSize: 18,
-    fontWeight: 'bold',
-    color: '#4A148C', // Dark purple for text
+    fontWeight: "bold",
+    color: "#4A148C", // Dark purple for text
   },
   assetValue: {
     fontSize: 16,
-    color: '#7B1FA2', // Medium purple for value
-    alignSelf: 'flex-end',
+    color: "#7B1FA2", // Medium purple for value
+    alignSelf: "flex-end",
   },
 });
 
