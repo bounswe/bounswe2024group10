ff45de31b8fd7daed3031adc20550efbb70cac60
OguzTNCR <100152836+OguzTNCR@users.noreply.github.com>
2024-10-21 20:51:55 +0300

    Improve postHeader ui

--- web/tradeverse/src/components/structure/postHeader.js
+++ web/tradeverse/src/components/structure/postHeader.js
@@ -1,15 +1,35 @@
-
 import React from "react";
-import "../styles/postHeader.css"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Link } from "react-router-dom";
+import {
+  faUser,
+  faCalendarAlt,
+  faThumbsUp,
+  faComments,
+} from "@fortawesome/free-solid-svg-icons";
+import "../styles/postHeader.css";
 
-
-const PostHeader = ({post}) => {
-    return (
-        <div className="postHeader">
-            <h3>{post.description}</h3> {/* Use the field you want to display as the header, e.g., post.title */}
-            <p>Posted by {post.username} on {post.date}</p>
+const PostHeader = ({ post }) => {
+  return (
+    <div className="postHeader">
+      <Link to={`/${post.forumName.toLowerCase()}/${post.id}`} className="postLink">
+        <div className="postInfo">
+          <h3>{post.description}</h3>{" "}
+          <p>
+            Posted by {post.username} on {post.date}
+          </p>
         </div>
-    );
-}
+      </Link>
+      <div className="postStats">
+        <p>
+          <FontAwesomeIcon icon={faThumbsUp} /> {post.likeCount}
+        </p>
+        <p>
+          <FontAwesomeIcon icon={faComments} /> {post.commentCount}
+        </p>
+      </div>
+    </div>
+  );
+};
 
-export default PostHeader
+export default PostHeader;
