cee8958811c5c30f1db6d8bfe1390910243890c9
omerfaruk <omer.erzurumluoglu@std.bogazici.edu.tr>
2024-11-20 22:38:54 +0300

    Update query in PostRepository to order posts by last edit date instead of creation date

--- backend/src/main/java/com/bounswe2024group10/Tradeverse/repository/PostRepository.java
+++ backend/src/main/java/com/bounswe2024group10/Tradeverse/repository/PostRepository.java
@@ -14,7 +14,7 @@ import com.bounswe2024group10.Tradeverse.model.Post;
 public interface PostRepository extends JpaRepository<Post, Long> {
     List<Post> findByUsername(String username);
     List<Post> findByParentID(Long parentID);
-    @Query("SELECT p FROM Post p WHERE p.title LIKE %:keyword% ORDER BY p.createdAt DESC")
+    @Query("SELECT p FROM Post p WHERE p.title LIKE %:keyword% ORDER BY p.lastEditDate DESC")
     List<Post> findByTitleContaining(@Param("keyword") String keyword, Pageable pageable);
 }
     
