b7952625f525f96eecbe710c0dc8d2f8a402c30e
omerfaruk <omer.erzurumluoglu@std.bogazici.edu.tr>
2024-11-21 13:59:45 +0300

    Permit access to user details API endpoint in SecurityConfig

--- backend/src/main/java/com/bounswe2024group10/Tradeverse/config/SecurityConfig.java
+++ backend/src/main/java/com/bounswe2024group10/Tradeverse/config/SecurityConfig.java
@@ -30,6 +30,7 @@ public class SecurityConfig {
                     .requestMatchers("/api/auth/get-user-details").permitAll()
                     .requestMatchers("/api/post/**").permitAll()
                     .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                    .requestMatchers("/api/user/get-user-details").permitAll()
                     .anyRequest().authenticated()
             );
         return http.build();
