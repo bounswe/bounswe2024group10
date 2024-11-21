00626e0db1a6bca80dd0afbbf87bb65a778541af
Gülşen Sabak <gulsensabak02@gmail.com>
2024-11-21 20:06:49 +0300

    feat: Implement like, dislike on posts
    (cherry picked from commit 891a6abb6efed14f9601ce98e2d65551b6e1ac15)

--- mobile/tradeverse/screens/home-root/_components/post-card.jsx
+++ mobile/tradeverse/screens/home-root/_components/post-card.jsx
@@ -165,7 +165,60 @@ const InteractionInfo = ({ icon = () => {}, value }) => (
 )
 
 export default function PostCard({ style, post }) {
+  {/* for handle likes*/}
+  const { user } = useContext(AuthContext)
+  const [isLiked, setIsLiked] = useState(post?.isLiked ?? false)
+
+  const handleLike = async () => {
+    const response = await likePost({
+      postId: post?.id,
+      username: user.username,
+    })
+    if (response.success) {
+      setIsLiked(true)
+    }
+  }
+
+
+  {/* for handle dislikes */}
+  const [isDisliked, setIsDisliked] = useState(post?.isDisliked ?? false);
+  const handleDislike = async () => {
+    const response = await dislikePost({
+      postId: post?.id,
+      username: user.username,
+    });
   
+    if (response?.success) {
+      setIsDisliked(true);
+      setIsLiked(false); // Disliking typically removes a like, adjust if behavior differs.
+    }
+  };
+
+  {/*for handle unlike*/}
+  const handleUnlike = async () => {
+    const response = await unlikePost({
+      postId: post?.id,
+      username: user.username,
+    });
+  
+    if (response?.success) {
+      setIsLiked(false);
+    }
+  };
+
+  {/* for handle undislike */}
+  const handleUndislike = async () => {
+    const response = await undislikePost({
+      postId: post?.id,
+      username: user.username,
+    });
+  
+    if (response?.success) {
+      setIsDisliked(false);
+    }
+  };
+
+
   return (
     <PostLink target={paths.HOME.POST_DETAIL} post={post}>
       <View
