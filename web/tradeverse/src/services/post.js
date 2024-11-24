4d42b8e0437a3b3ba51bde003f6bc1e132839cf5
OguzTNCR <100152836+OguzTNCR@users.noreply.github.com>
2024-11-24 22:02:16 +0300

    Add post service

---
+++ web/tradeverse/src/services/post.js
@@ -0,0 +1,22 @@
+import api from "./api";
+
+export async function getSubforums() {
+    try {
+        const response = await api.get("/post/get-subforums");
+        return response.data;
+    }
+    catch (error) {
+        console.error("Error fetching subforums:", error);
+    }
+
+}
+
+export async function createPost(postPayload) {
+    try {
+        const response = await api.post('/post/create-post', postPayload);
+        return response.data;
+    }
+    catch (error) {
+        console.error("Error creating post:", error);
+    }
+}
