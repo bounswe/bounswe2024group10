93dd544d03ab5f80d4499961bd67fc143d3d8f37
Gulsen <gulsensabak02@gmail.com>
2024-11-25 21:12:39 +0300

    fix: func call errors

--- mobile/tradeverse/services/user.js
+++ mobile/tradeverse/services/user.js
@@ -1,12 +1,13 @@
 import api from './_axios'
 
-export default async function getUserByUsername({ username, token }) {
+export async function getUserByUsername({ username, token }) {
   try {
-    const response = await api.post('/auth/get-user-details', { username });
+    if (!username) return null;
+    const response = await api.post('/auth/get-user-details', { username }, { headers: { Authorization: `Bearer ${token}` } });
     console.log(response.data);
     return response.data;
   } catch (error) {
     console.error('Get user by username failed', error)
+    return null
   }
-  return null
 }
