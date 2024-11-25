03d69e9266b4ba932e707d504a1913d86979ab49
Gulsen <gulsensabak02@gmail.com>
2024-11-25 20:59:24 +0300

    Fixed syntax errs

--- mobile/tradeverse/auth/context/auth-provider.jsx
+++ mobile/tradeverse/auth/context/auth-provider.jsx
@@ -4,14 +4,14 @@ import api from '../../services/_axios';
 import AuthContext from './auth-context';
 import { getMe, register, login } from '../../services/auth';
 import { getUserByUsername } from '../../services/user';
-
 import { Storage } from '../../util/storage';
+import AsyncStorage from '@react-native-async-storage/async-storage'; // Make sure to import AsyncStorage
 
 export default function AuthProvider({ children }) {
-  const [loading, setLoading] = useState(false)
-  const [user, setUser] = useState(null)
-  const [isLoggedin, setIsLoggedIn] = useState(false)
-  const [isTagSelected, setIsTagSelected] = useState(false)
+  const [loading, setLoading] = useState(false);
+  const [user, setUser] = useState(null);
+  const [isLoggedin, setIsLoggedIn] = useState(false);
+  const [isTagSelected, setIsTagSelected] = useState(false);
 
   const signIn = useCallback(async ({ username, password }) => {
     try {
@@ -19,7 +19,7 @@ export default function AuthProvider({ children }) {
       const res = await login({ username, password });
       if (res.data['isSuccessful'] === true) {
         await Storage.setItem('authToken', res.data?.token ?? "");
-        await Storage.setItem('username', res.data['username']?? "");
+        await Storage.setItem('username', res.data['username'] ?? "");
         const userProfile = await getUserByUsername({ username });
 
         setUser(userProfile);
@@ -27,24 +27,25 @@ export default function AuthProvider({ children }) {
         router.replace('(tabs)');
       }
     } catch (error) {
-      console.error(error.message)
+      console.error(error.message);
       throw new Error(error.message);
     } finally {
       setLoading(false);
     }
-  }, [])
+  }, []);
 
-  const signUp = useCallback(async ({ email, password, name, tag=0,profilePhoto, username }) => {
+  const signUp = useCallback(async ({ email, password, name, tag = 0, profilePhoto, username }) => {
     try {
-      // setLoading(true);
+      setLoading(true); // You might want to show a loading indicator
       const res = await register({
         email,
         password,
         name,
         tag,
         profilePhoto,
-        username
+        username,
       });
+
       if (res.status === 200) {
         await Storage.setItem('authToken', res.data?.token);
         await Storage.setItem('username', username);
@@ -53,92 +54,91 @@ export default function AuthProvider({ children }) {
         setUser(userProfile);
         setIsLoggedIn(true);
         router.replace('(tabs)');
-        // setUser(res.data?.user);
       }
-    },
-    []
-  )
+    } catch (error) {
+      console.error('Sign Up error:', error);
+    } finally {
+      setLoading(false);
+    }
+  }, []);
 
   const logout = useCallback(async () => {
-    setLoading(true);
-    setIsLoggedIn(false);
-    setUser(null);
-    router.replace('auth');
-    await Storage.removeItem('authToken');
-    await Storage.removeItem('username');
-    setLoading(false);
+    try {
+      setLoading(true);
+      setIsLoggedIn(false);
+      setUser(null);
+      router.replace('auth');
+      await Storage.removeItem('authToken');
+      await Storage.removeItem('username');
+    } catch (error) {
+      console.error('Logout error:', error);
+    } finally {
+      setLoading(false);
+    }
   }, []);
 
   const refetchUser = useCallback(async () => {
-    setLoading(true)
-
-    const token = await Storage.getItem('authToken');
-    const username = await Storage.getItem('username');
-
-    api.defaults.headers.common.Authorization = `Bearer ${token}`
-
-    const loggedinUser = await getMe({ authToken: token, username })
-
-    if (loggedinUser) {
-      setIsLoggedIn(true)
-      setUser(loggedinUser)
-    } else {
-      setUser(null)
-    }
-    setLoading(false)
-  }, [])
-
-  const setUserProfile = useCallback((profile) => {
-    setUser((prevUser) => ({ ...prevUser, profile }))
-  }, [])
-
-  const isAuthenticated = useCallback(async () => {
     try {
-      let result = false
-      setLoading(true)
+      setLoading(true);
 
-      const token = await AsyncStorage.getItem('authToken')
-      if (token) {
-        const validateResponse = await validateToken({ token })
-        if (validateResponse?.username) {
-          console.log('Token is valid ->', validateResponse, token)
-          api.defaults.headers.common.Authorization = `Bearer ${token}`
-          const userProfile = await getUserByUsername({
-            username: validateResponse.username,
-            token,
-          })
-          console.log('userProfile', userProfile)
+      const token = await Storage.getItem('authToken');
+      const username = await Storage.getItem('username');
 
-    const token = await Storage.getItem('authToken');
-    const username = await Storage.getItem('username');
+      api.defaults.headers.common.Authorization = `Bearer ${token}`;
 
-    api.defaults.headers.common.Authorization = `Bearer ${token}`;
-
-    if (token || username) {
       const loggedinUser = await getMe({ authToken: token, username });
+
       if (loggedinUser) {
         setIsLoggedIn(true);
         setUser(loggedinUser);
-        result = true;
       } else {
-        await Storage.removeItem('authToken');
-        setIsLoggedIn(false);
         setUser(null);
-        result = false;
       }
     } catch (error) {
-      console.log('error on isAuthenticated', error)
-      await AsyncStorage.removeItem('authToken')
-      setIsLoggedIn(false)
-      setUser(null)
+      console.error('Refetch User error:', error);
     } finally {
-      setLoading(false)
+      setLoading(false);
     }
-  }, [])
+  }, []);
+
+  const isAuthenticated = useCallback(async () => {
+    try {
+      setLoading(true);
+
+      const token = await AsyncStorage.getItem('authToken');
+      const username = await AsyncStorage.getItem('username');
+
+      if (token && username) {
+        api.defaults.headers.common.Authorization = `Bearer ${token}`;
+        const loggedinUser = await getMe({ authToken: token, username });
+
+        if (loggedinUser) {
+          setIsLoggedIn(true);
+          setUser(loggedinUser);
+        } else {
+          await AsyncStorage.removeItem('authToken');
+          await AsyncStorage.removeItem('username');
+          setIsLoggedIn(false);
+          setUser(null);
+        }
+      } else {
+        setIsLoggedIn(false);
+        setUser(null);
+      }
+    } catch (error) {
+      console.log('Error in isAuthenticated:', error);
+      await AsyncStorage.removeItem('authToken');
+      await AsyncStorage.removeItem('username');
+      setIsLoggedIn(false);
+      setUser(null);
+    } finally {
+      setLoading(false);
+    }
+  }, []);
 
   useEffect(() => {
-    isAuthenticated()
-  }, [isAuthenticated])
+    isAuthenticated();
+  }, [isAuthenticated]);
 
   const contextValue = useMemo(
     () => ({
@@ -157,19 +157,17 @@ export default function AuthProvider({ children }) {
     }),
     [
       user,
-      setUser,
       isLoggedin,
       loading,
       signIn,
-      refetchUser,
-      isTagSelected,
       setIsTagSelected,
+      refetchUser,
       signUp,
+      isTagSelected,
       logout,
+      setUser,
     ]
-  )
+  );
 
-  return (
-    <AuthContext.Provider value={contextValue}>{children}</AuthContext.Provider>
-  )
+  return <AuthContext.Provider value={contextValue}>{children}</AuthContext.Provider>;
 }
