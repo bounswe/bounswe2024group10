da6ee4b6173746292d7dbe74a87bb7528f2b7343
melihakpinar <mesutmelihakpinar@gmail.com>
2024-12-09 04:57:38 +0300

    Implement repositories for posts, comments, and subforums

---
+++ backend/src/main/java/com/bounswe2024group10/Tradeverse/repository/CommentRepository.java
@@ -0,0 +1,13 @@
+package com.bounswe2024group10.Tradeverse.repository;
+
+import com.bounswe2024group10.Tradeverse.model.Comment;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+    List<Comment> findByParentCommentID(Long parentCommentID);
+    List<Comment> findByPostID(String postID);
+    List<Comment> findByCreatedBy(String createdBy);
+} 
--- backend/src/main/java/com/bounswe2024group10/Tradeverse/repository/PostRepository.java
+++ backend/src/main/java/com/bounswe2024group10/Tradeverse/repository/PostRepository.java
@@ -1,10 +1,12 @@
 package com.bounswe2024group10.Tradeverse.repository;
 
 import com.bounswe2024group10.Tradeverse.model.Post;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
     Post findBySubforumID(Long subforumID);
+    List<Post> findByCreatedBy(String createdBy);
 }
