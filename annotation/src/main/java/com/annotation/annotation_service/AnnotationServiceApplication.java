82031c0db1cbb4b511b4bbf88a4f61cc7ae8f1ad
OguzTNCR <100152836+OguzTNCR@users.noreply.github.com>
2024-12-13 15:56:49 +0300

    initialize springboot framework

---
+++ annotation/src/main/java/com/annotation/annotation_service/AnnotationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.annotation.annotation_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnnotationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnnotationServiceApplication.class, args);
+	}
+
+}
---
+++ annotation/src/test/java/com/annotation/annotation_service/AnnotationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.annotation.annotation_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnnotationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
