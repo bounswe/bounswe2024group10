0cfcd9385ee2c33daf88f3c1ffa5298e272ecdfb
karatashsyn <81769362+karatashsyn@users.noreply.github.com>
2024-12-15 23:53:46 +0300

    refactor: Refactor Result Cards on Search

--- mobile/tradeverse/services/user.js
+++ mobile/tradeverse/services/user.js
@@ -1,7 +1,6 @@
 import api from './_axios'
-import AsyncStorage from '@react-native-async-storage/async-storage'
 
-export default async function getUserByUsername({ username, token }) {
+export async function getUserByUsername({ username, token }) {
   try {
     const response = await api({
       url: `/user/get-user-details/${username}`,
@@ -19,20 +18,19 @@ export default async function getUserByUsername({ username, token }) {
 
 export async function setProfile({ email, profilePhoto, bio, tag, username }) {
   try {
-    const token = await AsyncStorage.getItem('authToken')
-    console.log(token)
-    const response = await api.post(`/user/set-user-details/${username}`, 
+    const response = await api.post(
+      `/user/set-user-details/${username}`,
       {
         email,
         profilePhoto,
         bio,
         tag,
-      }, 
+      },
       {
-        headers: { Authorization: `Bearer ${token}` },
         maxBodyLength: Infinity,
-      })
-  
+      }
+    )
+
     return response.data
   } catch (error) {
     console.log('Set profile failed', error)
